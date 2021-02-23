d03d521be867554ecd552c378d57af3b0d13f680
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-23 15:50:14 +0000

    Create SaintValidationException.java

---
+++ src/com.ait.validation/SaintValidationException.java
@@ -0,0 +1,11 @@
+package com.ait.validation;
+
+public class SaintValidationException extends SaintException {
+
+	private static final long serialVersionUID = 334051992916748022L;
+
+	public SaintValidationException(final String errorMessage) {
+		super(errorMessage);
+	}
+
+}
