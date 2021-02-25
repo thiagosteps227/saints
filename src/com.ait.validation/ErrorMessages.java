fea70d9dd1556e414b52e0c8102dfff510589e6b
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-25 12:16:56 +0000

    Update ErrorMessages.java

--- src/com.ait.validation/ErrorMessages.java
+++ src/com.ait.validation/ErrorMessages.java
@@ -1,7 +1,9 @@
 package com.ait.validation;
 
 public enum ErrorMessages {
-	EMPTY_FIELDS("One or more empty fields");
+	EMPTY_FIELDS("One or more empty fields"),
+	ALREADY_EXISTS("The saint with given name already exists"),
+	INVALID_CENTURY("Type a valid century between 0 and 21");
 	
 	
 	private String errorMessage;
