7cbc8b75d1dc631ded213f6abdecc33926ed69e6
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-03-02 15:38:13 +0000

    integration test

---
+++ src/com.ait.tests/IntegrationTests.java
@@ -0,0 +1,61 @@
+package com.ait.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.core.Response;
+import org.apache.commons.httpclient.HttpStatus;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.ait.saints.Saints;
+import com.ait.saints.SaintsDAO;
+import com.ait.saints.SaintsResource;
+
+class IntegrationTests {
+	
+	SaintsResource saintsResource;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		saintsResource = new SaintsResource();
+		ResetTable reset = new ResetTable();
+		List<Saints> saints = new ArrayList<Saints>();
+		Saints saint = new Saints();
+		saint.setName("test");
+		saint.setCountry("country");
+		saint.setCity("city");
+		saint.setPicture("saint.jpg");
+		saint.setDescription("saint created for test");
+		saint.setCentury(1);
+		saints.add(saint);
+		saint = new Saints();
+		saint.setName("test2");
+		saint.setCountry("country2");
+		saint.setCity("city2");
+		saint.setPicture("saint2.jpg");
+		saint.setDescription("saint created for test2");
+		saint.setCentury(2);
+		saints.add(saint);
+		reset.resetTable(saints);
+		
+	}
+	
+	@Test
+	void testGetAllSaints() {
+		Response response= saintsResource.findAll();
+		List<Saints> list =  (List<Saints>) response.getEntity();
+		assertEquals(HttpStatus.SC_OK, response.getStatus());
+		assertEquals(2, list.size());
+		Saints saint= list.get(0);
+		assertEquals("country", saint.getCountry());
+
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	
+
+}
