342324463a651a9fb558487e4bbd40d902628b34
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-03-03 09:17:55 +0000

    Update SaintsDAO.java

--- src/com.ait.saints/SaintsDAO.java
+++ src/com.ait.saints/SaintsDAO.java
@@ -1,181 +1,99 @@
 package com.ait.saints;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import com.mysql.jdbc.PreparedStatement;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.ait.validation.ErrorMessages;
+import com.ait.validation.SaintValidationException;
+import com.ait.validation.SaintsValidator;
+import com.ait.validation.*;
 
 
-public class SaintsDAO {
-
-	public List<Saints> findAll() {
-        List<Saints> list = new ArrayList<Saints>();
-        Connection c = null;
-    	String sql = "SELECT * FROM saints ORDER BY id";
-        try {
-            c = ConnectionHelper.getConnection();
-            Statement s = c.createStatement();
-            ResultSet rs = s.executeQuery(sql);
-            while (rs.next()) {
-                list.add(processRow(rs));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-		} finally {
-			ConnectionHelper.close(c);
+@Path("/saints")
+public class SaintsResource {
+	
+	SaintsValidator saintsValidator = new SaintsValidator();
+	SaintsDAO saintsDAO = new SaintsDAO();
+	CheckSaintExists checkSaintExists = new CheckSaintExists();
+	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response findAll() {
+		List<Saints> list = saintsDAO.findAll();
+		return Response.status(200).entity(list).build();
+	}
+	
+	@GET @Path("{id}")
+	@Produces({ MediaType.APPLICATION_JSON})
+	//http://localhost:8080/Saints/rest/saints/{id}
+	public Response findById(@PathParam("id") int id) {
+		Saints saint = saintsDAO.findById(id);
+		return Response.status(200).entity(saint).build();
+	}
+	
+	@GET @Path("search/{query}")
+	@Produces({MediaType.APPLICATION_JSON})
+	//http://localhost:8080/wines/search/{query}
+	public Response findByName(@PathParam("query") String name) {
+		List<Saints> saint = saintsDAO.findByName(name);
+		return Response.status(200).entity(saint).build();
+	}
+	
+	@GET @Path("/query")
+	@Produces({MediaType.APPLICATION_JSON})
+	//http://localhost:8080/Saints/rest/query?country=Turkey&century=1
+	public Response findByCountryAndCentury(
+			@QueryParam("country") String country,
+			@QueryParam("century") String century){
+		List<Saints> saints = saintsDAO.findByCountryAndCentury(country, century);
+		return Response.status(200).entity(saints).build();
 		}
-        return list;
-    }
 	
-	public Saints findById(int id) {
-    	String sql = "SELECT * FROM saints WHERE id=?";
-    	Saints saint = null;
-    	Connection c = null;
-    	try {
-    		c = ConnectionHelper.getConnection();
-    		PreparedStatement ps = (PreparedStatement) c.prepareStatement(sql);
-    		ps.setInt(1, id);
-    		ResultSet rs = ps.executeQuery();
-    		if(rs.next()) {
-    			saint = processRow(rs);
-    		}
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    		throw new RuntimeException(e);
-    	} finally {
-    		ConnectionHelper.close(c);
-    	}
-    	return saint;
-    }
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
+	//http://localhost:8080/Saints/rest/saints
+	public Response create(Saints saint) {
+		Response response;
+		try {
+			List<Saints> saints = SaintsDAO.findAll();
+			saintsValidator.checkEmptyFields(saint);
+			checkSaintExists.checkSaintExists(saint, saints);
+			SaintsDAO.create(saint);
+			response = Response.status(200).entity(saint).build();
+		} catch (SaintValidationException e) {
+			ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
+			response = Response.status(403).entity(errorMessage).build();
+		}
+		
+		return response;
+	}
 	
-	 public List<Saints> findByName(String name) {
-	    	List<Saints> list = new ArrayList<Saints>();
-	    	Connection c= null;
-	    	String sql = "SELECT * FROM saints AS e WHERE UPPER(name) LIKE ? ORDER BY name";
-	    	try {
-	    		c = ConnectionHelper.getConnection();
-	    		java.sql.PreparedStatement ps = c.prepareStatement(sql);
-	    		ps.setString(1, "%"+name.toUpperCase()+"%");
-	    		ResultSet rs = ps.executeQuery();
-	    		while(rs.next()) {
-	    			list.add(processRow(rs));
-	    		}
-	    	} catch (SQLException e){
-	    		e.printStackTrace();
-	    		throw new RuntimeException(e);
-	    	} finally {
-	    		ConnectionHelper.close(c);
-	    	}
-	    	return list;
-	    }
-	 
-	 public List<Saints> findByCountryAndCentury(String country, String century){
-	    	List<Saints> list = new ArrayList<Saints>();
-	    	Connection c= null;
-	    	String sql = "SELECT * FROM saints AS e WHERE UPPER(country) LIKE ? AND UPPER(century) LIKE ? "
-	    			+ "ORDER BY name";
-	    	try {
-	    		c = ConnectionHelper.getConnection();
-	    		PreparedStatement ps = (PreparedStatement) c.prepareStatement(sql);
-	    		ps.setString(1, "%"+country.toUpperCase()+"%");
-	    		ps.setString(2, "%"+century.toUpperCase()+"%");
-	    		ResultSet rs = ps.executeQuery();
-	    		while(rs.next()) {
-	    			list.add(processRow(rs));
-	    		}
-	    	} catch (SQLException e) {
-	    		e.printStackTrace();
-	    		throw new RuntimeException(e);
-	    	} finally {
-	    	ConnectionHelper.close(c);
-	    	}
-	    	return list;
-	    }
-	 
-	 public Saints create(Saints saint) {
-	    	Connection c= null;
-	    	java.sql.PreparedStatement ps = null;
-	    	try {
-	    		c = ConnectionHelper.getConnection();
-	    		ps = c.prepareStatement("INSERT INTO saints "
-	    				+ "(name, country, city, picture, description, century)"
-	    				+ "VALUES(?,?,?,?,?,?)", 
-	    			new String[] { "id"});
-	    		ps.setString(1, saint.getName());
-	    		ps.setString(2, saint.getCountry());
-	    		ps.setString(3, saint.getCity());
-	    		ps.setString(4, saint.getPicture());
-	    		ps.setString(5, saint.getDescription());
-	    		ps.setInt(6, saint.getCentury());
-	    		ps.executeUpdate();
-	    		ResultSet rs = ps.getGeneratedKeys();
-	    		rs.next();
-	    		int id = rs.getInt(1);
-	    		saint.setId(id);
-	    	} catch (Exception e){
-	    		e.printStackTrace();
-	    		throw new RuntimeException(e);
-	    	} finally {
-	    		ConnectionHelper.close(c);
-	    	}
-	    	
-	    	return saint;
-	    }
-	 
-	 public Saints update(Saints saint) {
-	    	Connection c = null;
-	    	try {
-	    		c = ConnectionHelper.getConnection();
-	    		PreparedStatement ps = (PreparedStatement) c.prepareStatement("UPDATE saints SET name=?, country=?, city=?,"
-	    				+ "picture=?, description=?, century=? WHERE id=?");
-	    		ps.setString(1, saint.getName());
-	    		ps.setString(2, saint.getCountry());
-	    		ps.setString(3, saint.getCity());
-	    		ps.setString(4, saint.getPicture());
-	    		ps.setString(5, saint.getDescription());
-	    		ps.setInt(6, saint.getCentury());
-	    		ps.setInt(7, saint.getId());
-	    		ps.executeUpdate();
-	    	} catch (SQLException e) {
-	    		e.printStackTrace();
-	    		throw new RuntimeException (e);
-	    	} finally {
-	    		ConnectionHelper.close(c);
-	    	}
-	    	return saint;
-	    }
-	 
-	  public boolean remove(int id) {
-	    	Connection c= null;
-	    	try {
-	    		c = ConnectionHelper.getConnection();
-	    		PreparedStatement ps = (PreparedStatement) c.prepareStatement("DELETE FROM saints WHERE id =?");
-	    		ps.setInt(1, id);
-	    		int count = ps.executeUpdate();
-				return count == 1;
-	    	} catch (Exception e) {
-	    		e.printStackTrace();
-	    		throw new RuntimeException (e);
-	    	} finally {
-	    		ConnectionHelper.close(c);
-	    	}
-	    }
+	@PUT @Path("{id}")
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
+	//http://localhost:8080/Saints/rest/{id}
+	public Response update(Saints saint) {
+		saintsDAO.update(saint);
+		return Response.status(201).entity(saint).build();
+	}
 	
-	protected Saints processRow(ResultSet rs) throws SQLException {
-        Saints saint = new Saints();
-        saint.setId(rs.getInt("id"));
-        saint.setName(rs.getString("name"));
-        saint.setCountry(rs.getString("country"));
-        saint.setCity(rs.getString("city"));
-        saint.setCentury(rs.getInt("century"));
-        saint.setPicture(rs.getString("picture"));
-        saint.setDescription(rs.getString("description"));
-        return saint;
-    }
+	@DELETE @Path("{id}")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response remove(@PathParam("id") int id) {
+		saintsDAO.remove(id);
+		return Response.status(204).build();
+	}
 }
-
-	
