9538ede132ebb88b53656058219b0afb67a54b1c
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-03-11 15:47:01 +0000

    UI with bootstrap

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,18 +1,46 @@
-let rootURL = "http://localhost:8080/Saints/rest/saints";
+// The root URL for the RESTful services
+var rootURL = "http://localhost:8080/WineCellar/rest/wines";
 
-let findAll = function(){
-	$.ajax({type: 'GET',
+
+var findAll= function () {
+	console.log('findAll');
+	$.ajax({
+		type: 'GET',
 		url: rootURL,
-		datatype: "json",
-		success:renderList});
+		dataType: "json", // data type of response
+		success: renderList
+	});
+};
+
+
+var renderList = function (list) {
+	//list=data.wine;
+        console.log("response");
+	$.each(list, function(index, wine) {
+		$('#table_body').append('<tr><td>'+wine.name+'</td><td>'+
+				wine.grapes+'</td><td>'+wine.country+'</td><td>'+wine.year+
+				'</td></tr>');
+	});
+        $('#table_id').DataTable();
+};
+
+var openModal = function(){
+	$('#name').val("BLOCK NINE");
+	$('#grapes').val("Merlot");
+	$('#country').val("Ireland");
+	$('#region').val("Athlone");
+	$('#year').val("2021");
+	$('#myModal').modal('show');
 }
 
-function renderList(saints){
-	$.each(saintList, function(index,saints){
-		$('#saintList').append('<li><a href="#" '+ saints.id+'">'+saints.name+'</a></li>');
-	})
-}
-
+//Retrieve the wine list when the DOM is ready
 $(document).ready(function(){
+	$(document).on("click", '#modalBtn', function(){openModal();});
 	findAll();
-})
+	
+       // $('#tab a').click(function (e) {
+       //     e.preventDefault();
+        //    $(this).tab('show');
+       // });
+});
+
