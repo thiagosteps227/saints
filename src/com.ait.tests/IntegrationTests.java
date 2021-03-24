d705bbf6e0bffcd7582e088d0d1fa9ced1971ce2
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-03-24 10:16:28 +0000

    with the recovering data

--- src/com.ait.tests/IntegrationTests.java
+++ src/com.ait.tests/IntegrationTests.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.core.Response;
 import org.apache.commons.httpclient.HttpStatus;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.ait.saints.Saints;
 import com.ait.saints.SaintsDAO;
 import com.ait.saints.SaintsResource;
+import com.ait.validation.ErrorMessage;
 
 class IntegrationTests {
 	
@@ -58,8 +60,119 @@ class IntegrationTests {
 
 	}
 	
-	@AfterEach
-	void tearDown() throws Exception {
+	@Test
+	public void testAddSaint() {
+		Saints saint = new Saints();
+		saint.setName("test3");
+		saint.setCountry("country3");
+		saint.setCity("city3");
+		saint.setPicture("saint3.jpg");
+		saint.setDescription("saint created for test3");
+		saint.setCentury(1);
+		Response response= saintsResource.create(saint);
+		assertEquals(HttpStatus.SC_OK, response.getStatus());
+		saint = (Saints) response.getEntity();
+		assertEquals(saint.getName(), "test3");
+		response= (Response) saintsResource.findAll();
+		List<Saints> list = (List<Saints>) response.getEntity();
+		assertEquals(list.size(), 3);
+	}
+	
+	@Test
+	public void testSaintAlreadyExistsException() {
+		Saints saint = new Saints();
+		saint.setName("test");
+		saint.setCountry("country");
+		saint.setCity("city");
+		saint.setPicture("saint.jpg");
+		saint.setDescription("saint created for test");
+		saint.setCentury(1);
+		Response response= saintsResource.create(saint);
+		assertEquals(HttpStatus.SC_FORBIDDEN, response.getStatus());
+		ErrorMessage message = (ErrorMessage) response.getEntity();
+		assertEquals(message.getErrorMessage(), "The saint with given name already exists");
+		response= (Response) saintsResource.findAll();
+		List<Saints> list = (List<Saints>) response.getEntity();
+		assertEquals(list.size(), 2);
+	}
+	
+	@Test
+	public void testAddWineWithInvalidCentury() {
+		Saints saint = new Saints();
+		saint.setName("test3");
+		saint.setCountry("country3");
+		saint.setCity("city3");
+		saint.setPicture("saint3.jpg");
+		saint.setDescription("saint created for test3");
+		saint.setCentury(22);
+		Response response= saintsResource.create(saint);
+		assertEquals(HttpStatus.SC_FORBIDDEN, response.getStatus());
+		ErrorMessage message = (ErrorMessage) response.getEntity();
+		assertEquals(message.getErrorMessage(), "Type a valid century between 1 and 21");
+		response= (Response) saintsResource.findAll();
+		List<Saints> list = (List<Saints>) response.getEntity();
+		assertEquals(list.size(), 2);
+	}
+	
+	@Test 
+	public void testfindById() {
+		Response response= saintsResource.findById(1);
+		Saints saint =  (Saints) response.getEntity();
+		assertEquals(HttpStatus.SC_OK, response.getStatus());
+		assertEquals(1, saint.getId());
+		
+	}
+	
+	@Test
+	public void testFindByName() {
+		Response response = saintsResource.findByName("test");
+		List<Saints> list = (List<Saints>) response.getEntity();
+		assertEquals(HttpStatus.SC_OK, response.getStatus());
+		Saints saint = list.get(0);
+		assertEquals("test", saint.getName());
+	
+	}
+	
+	@Test
+	public void testFindByCountryAndCentury() {
+		Response response = saintsResource.findByCountryAndCentury("country", 1);
+		List<Saints> list = (List<Saints>) response.getEntity();
+		assertEquals(HttpStatus.SC_OK, response.getStatus());
+		Saints saint = list.get(0);
+		assertEquals("country", saint.getCountry());
+		assertEquals(1, saint.getCentury());
+	
+	}
+	
+	@Test
+	public void testUpdateSaint() {
+		List<Saints> list = SaintsDAO.findAll();
+		Saints saint = list.get(0);
+		assertEquals("test", saint.getName());
+		saint.setName("updated");
+		Response response = saintsResource.update(saint);
+		Saints saintUpdated = (Saints) response.getEntity();
+		assertEquals(HttpStatus.SC_CREATED, response.getStatus());
+		assertEquals("updated", saintUpdated.getName());
+	
+	}
+	
+	@Test
+	public void testDeleteSaint() {
+		Saints saint = SaintsDAO.findById(1);
+		assertEquals("test", saint.getName());
+		Response response = saintsResource.remove(1);
+		assertEquals(HttpStatus.SC_NO_CONTENT, response.getStatus());
+			
+	}
+	
+	@AfterAll
+	public static void backToNormal(){
+		
+		ResetTable recovering = new ResetTable();
+		recovering.recoverData();
+
+		
 	}
 
 	
